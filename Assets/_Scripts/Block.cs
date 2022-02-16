69c3777c21435e00600eff4a5bf22a3d39d7f067
Pedro Lucas <pedrolucassinuso@gmail.com>
2022-02-16 02:04:57 -0300

    Base Gaming Setup
    2048 base game by Tarodev

---
+++ Assets/_Scripts/Block.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Block : MonoBehaviour
+{   
+    public Vector2 Pos => transform.position; 
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private TextMeshPro _text;
+    public int Value;
+    public Node Node;
+    public Block MergingBlock;
+    public bool Merging;
+
+    public void Init(BlockType type){
+        Value = type.Value;
+        //_renderer.sprite = type.Sprite;
+        _renderer.color = type.Color;
+        _text.text = type.Value.ToString();
+    }
+
+    public void SetBlock(Node node){
+        if(Node != null) Node.OccupiedBlock = null;
+        Node = node;
+        Node.OccupiedBlock = this;
+    }
+
+    public void MergeBlock (Block blockToMergeWith){
+
+        MergingBlock = blockToMergeWith;
+
+        Node.OccupiedBlock = null;
+
+        blockToMergeWith.Merging = true;
+
+    }
+
+    public bool CanMerge(int value) => value == Value && !Merging && MergingBlock == null;
+}
